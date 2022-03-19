b484f3b2f5d9fcb7dc01e21d3f7356f4959d7bcc
Alexander Kovzhut <akovzhut@icloud.com>
2022-03-19 21:37:27 +0300

    Initial commit
    Create empty project + delete storyboard

--- BTCWallet/ViewControllers/RootViewController.swift
+++ BTCWallet/ViewControllers/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  BTCWallet
 //
 //  Created by Alexander Kovzhut on 19.03.2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RootViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#function)
+        
+        view.backgroundColor = .blue
     }
-
-
+    
 }
-
---
+++ BTCWallet/ViewControllers/WalletInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WalletInfoViewController.swift
+//  BTCWallet
+//
+//  Created by Alexander Kovzhut on 19.03.2022.
+//
+
+import UIKit
+
+class WalletInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+        
+        view.backgroundColor = .green
+    }
+    
+}

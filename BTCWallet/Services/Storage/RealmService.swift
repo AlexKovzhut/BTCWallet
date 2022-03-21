893d36a9f1fcdd88b057da13f1e0e186f63086ef
Alexander Kovzhut <akovzhut@icloud.com>
2022-03-22 01:34:21 +0300

    Add realm
    Add real (SPM)

---
+++ BTCWallet/Services/Storage/RealmService.swift
@@ -0,0 +1,18 @@
+//
+//  RealmService.swift
+//  BTCWallet
+//
+//  Created by Alexander Kovzhut on 21.03.2022.
+//
+
+import UIKit
+import RealmSwift
+
+class RealmService {
+    static let shared = RealmService()
+    
+    let realm = try! Realm()
+    
+    
+    
+}
